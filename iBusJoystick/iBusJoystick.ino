350337da8699baf9eae1f3b602767a42fc0e359d
Tom Varghese <tomxposed@gmail.com>
2020-06-26 08:39:09 +0530

    Code Cleanup

--- iBusJoystick/iBusJoystick.ino
+++ iBusJoystick/iBusJoystick.ino
@@ -1,10 +1,8 @@
 #include <Joystick.h>
 #include "FlySkyIBus.h"
 
-volatile int values[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
 // Joystick setup object
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_MULTI_AXIS, 32, 0,true, true, false, false, false, false, true, true, false, false, false);
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_MULTI_AXIS, 32, 0, true, true, false, false, false, false, true, true, false, false, false);
 //                ( HID report ID             Joystick Type,           btn hat  X     Y     Z      Rx     Ry     Rz   Rudd   Thro  Acc   Break  Steer)
 
 void setup()
@@ -12,6 +10,7 @@ void setup()
   Joystick.begin(true);
   Serial1.begin(115200);
   IBus.begin(Serial1);
+  
   Joystick.setXAxisRange(0, 1023);
   Joystick.setYAxisRange(0, 1023);
   Joystick.setThrottleRange(0, 1023);
@@ -21,30 +20,16 @@ void setup()
 void loop()
 {
   IBus.loop();
-  // Read and set states from all channels
-  for (int i = 0; i < 8; i++) {
-    values[i] = IBus.readChannel(i);
-
-  }
-
-  // Get positions values
-  values[0] = map(values[0], 1000, 2000, 0, 1023);
-  values[1] = map(values[1], 1000, 2000, 0, 1023);
-  values[2] = map(values[2], 1000, 2000, 0, 1023);
-  values[3] = map(values[3], 1000, 2000, 0, 1023);
-  values[4] = map(values[4], 1000, 2000, 0, 1);
-  values[5] = map(values[5], 1000, 2000, 0, 1);
-  values[6] = map(values[6], 1000, 1500, 0, 1);
-  values[7] = map(values[7], 1000, 2000, 0, 1);
+  
+  // Get iBus values and map to supported ranges
+  int roll = map(IBus.readChannel(0), 1000, 2000, 0, 1023);
+  int pitch = map(IBus.readChannel(1), 1000, 2000, 0, 1023);
+  int throttle = map(IBus.readChannel(2), 1000, 2000, 0, 1023);
+  int yaw = map(IBus.readChannel(3), 1000, 2000, 0, 1023);
 
   // Map values to the Joystick object
-  Joystick.setXAxis(values[0]);
-  Joystick.setYAxis(values[1]);
-  Joystick.setThrottle(values[2]);
-  Joystick.setRudder(values[3]);
-  Joystick.setButton(0, values[4]);
-  Joystick.setButton(1, values[5]);
-  Joystick.setButton(2, values[6]);
-  Joystick.setButton(3, values[7]);
-
+  Joystick.setXAxis(roll);
+  Joystick.setYAxis(pitch);
+  Joystick.setThrottle(throttle);
+  Joystick.setRudder(yaw);
 }
